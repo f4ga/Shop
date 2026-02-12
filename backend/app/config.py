d1305a43f6aa2970b52f54f54543ea4fac79e4dc
f4lga <rinagodulyan@gmail.com>
2026-02-12 20:25:07 +0300

    init

---
+++ backend/app/config.py
@@ -0,0 +1,18 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    app_name: str = "my shop"
+    debug: bool = True
+    db_url: str = "sqlite:///./shop.db"
+    cors_origins: list[str] = [
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "http://127.0.0.1:5173",
+        "http://127.0.0.1:3000",
+    ]
+    static_dir: str = "static"
+    images_dir: str = "static/images"
+
+    class Config:
+        env_file = ".env"
